9c30d8f0fa395239c5e668b7bc13a9b7417f3a9b
patricebonraisin <157237184+patricebonraisin@users.noreply.github.com>
2026-05-20 10:52:54 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "pato-apps-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "patolife.png",
+  "melovia.png",
+  "finelio.png",
+  "loopamove.png",
+  "pato-life-blog-page.html",
+  "melovia-blog-page.html",
+  "finelio-blog-page.html",
+  "loopa-move-blog-page.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
