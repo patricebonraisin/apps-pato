8888ebf0273d2dc328a8aaf385e5776c65b887fe
patricebonraisin <157237184+patricebonraisin@users.noreply.github.com>
2026-08-25 11:54:40 +0200

    Modification et MAJ de la Web app

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,63 @@
-const CACHE_NAME = "pato-apps-v3";
+const CACHE_NAME = "pato-apps-v4";
 
 const FILES_TO_CACHE = [
   "./",
   "index.html",
+  "manifest.json",
+  "accueil.png",
+  "app-map.png",
+  "ascens-logo.png",
+  "hero-climber.png",
   "patolife.png",
   "melovia.png",
   "finelio.png",
   "loopamove.png",
   "encore_la.png",
   "pato-meteo.png",
+  "pulse_game.png",
+  "ascens_marketing.html",
+  "ascens_privacy.html",
+  "ascens_support.html",
   "pato-life-blog-page.html",
   "melovia-blog-page.html",
   "finelio-blog-page.html",
   "loopa-move-blog-page.html",
   "page-marketing-encore-la.html",
-  "Pato_meteo_MarketingPage.html"
+  "Pato_meteo_MarketingPage.html",
+  "Pato_meteo_PrivacyPolicy.html",
+  "pulse_marketing.html"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestURL = new URL(event.request.url);
+  if (requestURL.origin !== self.location.origin) {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
